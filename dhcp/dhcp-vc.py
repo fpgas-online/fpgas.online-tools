3243f97af5d1ff87550ee0ef1fb429d98b6e1c3e
Carl Karsten <carl@personnelware.com>
2024-01-27 04:26:02 -0600

    update docs

---
+++ dhcp/dhcp-vc.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+import argparse
+import datetime
+import json
+
+from collections import defaultdict
+from pprint import pprint
+
+def get_log(args):
+
+    logs = []
+    with open(args.filename,'r') as f:
+        for line in f:
+            line = line.strip('\n')
+            d = json.loads(line)
+            logs.append(d)
+
+    return logs
+
+def get_answers(logs):
+
+    def remove_roages(logs):
+        """
+        fn='/home/carl/src/tv/pib/pici/ansible/inventory/host_vars/ps1.fpgas.mithis.com'
+        s=open(fn).read()
+
+        pi_macs=['b8:27:eb:2f:5d:08', 'dc:a6:32:05:32:45', 'b8:27:eb:d4:f1:74', 'b8:27:eb:33:51:27', 'b8:27:eb:a3:51:b4', 'b8:27:eb:51:01:df', 'b8:27:eb:68:fc:e7', 'b8:27:eb:69:79:a0', 'b8:27:eb:5f:de:85', 'b8:27:eb:0c:f8:43']
+        wonky_macs= [ "b8:27:eb:5a:26:5b", "e4:5f:01:64:75:c6" '46:63:56:3c:80:85' ]
+
+        logs = [
+                log for log in logs
+                    if log['mac'] in pi_macs
+                ]
+
+        return logs
+
+
+    def spackle(logs):
+        mac_to_hostname=defaultdict()
+
+        # find all the mac:hostnames that exist
+        for d in logs:
+            if d['hostname'] is not None:
+                mac_to_hostname[d['mac']] =  d['hostname']
+
+        # fill in all the hostnames
+        for d in logs:
+            if d['mac'] in mac_to_hostname:
+                d['host_name'] = mac_to_hostname[d['mac']]
+            else:
+                print(f"roage mac: {d['mac']}")
+                pprint(d)
+
+    logs = remove_roages(logs)
+    logs=spackle(logs)
+
+    ret = {}
+
+    # pis=set()
+    vcs=set()
+    stats={} # defaultdict(defaultdict(int))
+
+    for d in logs:
+
+        # pis.add(d['hostname'])
+        vcs.add(d['vendor_class'])
+
+        if d['mac'] not in stats:
+            stats[d['mac']] = defaultdict(int)
+
+        stats[d['mac']][d['vendor_class']] +=1
+
+
+    # ret['pis'] = pis
+    ret['vcs'] = vcs
+    ret['stats'] = stats
+    # ret['mac_to_hostname'] = mac_to_hostname
+
+    return ret
+
+
+
+def focus(answers, args):
+
+    if args.hostname:
+        # answers = [d for d in answers if d['hostname'] == args.hostname ]
+        answers = filter( lambda d: d['hostname'] == args.hostname, answers )
+
+    return answers
+
+
+def show_answers(answers):
+
+    stats = answers['stats']
+    for k in stats:
+
+        host_name = stats[k]['hostname']
+
+        if host_name[:2] == 'pi':
+            print(host_name)
+            pprint(stats[k])
+            print()
+
+
+
+def debug(args):
+
+    with open('/tmp/foo','a') as f:
+        f.write(args.__repr__())
+        f.write('\n')
+        f.write(os.getenv('DNSMASQ_SUPPLIED_HOSTNAME', "oh no!"))
+        f.write('\n')
+        f.write(os.getenv('DNSMASQ_VENDOR_CLASS', "oh noze!"))
+        f.write('\n')
+
+
+def get_args():
+
+    """
+    The  arguments to the process are
+    action: one of ["add", "old", "del"],
+    MAC address of the host (or DUID for IPv6) ,
+    IP address,
+    hostname, if known.
+    """
+
+    parser = argparse.ArgumentParser(
+            description="""called from dnsmasq""")
+
+    parser.add_argument('--filename',
+            default="vc.jsons",
+            help='File to save mac/hostnames (overide for testing)')
+
+    parser.add_argument('--host',
+            help="only show [hostname]")
+
+    parser.add_argument('--debug',
+            default=False,
+            help="append 'rawr' data to /tmp/foo")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    if args.debug:
+        debug(args)
+
+    logs = get_log(args)
+    # pprint(logs[:3])
+
+    answers = get_answers(logs)
+    show_answers(answers)
+
+if __name__ == "__main__":
+    main()
+
---
+++ dhcp/log_lookie.py
@@ -0,0 +1,163 @@
+import re
+
+from collections import defaultdict
+from pprint import pprint
+
+fn = '/home/carl/temp/syslog'
+
+ll = "Feb 12 14:45:46 val2 dnsmasq-dhcp[215151]: 2603362573 vendor class: Linux ipconfig"
+
+syslog_re_s = r"""
+# https://stackoverflow.com/questions/53435437/parsing-a-syslog-using-regex
+(?P<month>\S{3})                    #
+\s+                              # added + because single digit dates might have additional spaces
+(?P<date>[0-9]{1,2})              # changed {2}? to {1,2} because you might have one or two digits
+\s+                              #
+(?P<time>[0-9]+:[0-9]+:[0-9]+)    #
+\s+                              #
+(?P<hostname>\S+)                 # anything which isn't whitespace
+\s+                              #
+(?P<daemon>[^\[]+)                   # just in case your daemon has a digit or lower case in its name
+\[                                #
+(?P<pid>\d+)               #
+\]                                #
+:                                #
+\s+                              #
+"""
+syslog_re_s = ''.join([l.split('#')[0].strip() for l in syslog_re_s.split('\n')])
+print(syslog_re_s)
+syslog_re = re.compile(syslog_re_s)
+print(syslog_re)
+print(ll)
+match = syslog_re.match(ll)
+d = match.groupdict()
+pprint(d)
+print("{month} {date} {time}".format(**d))
+
+context_id_re = re.compile(syslog_re_s + r"(?P<context_id>\d+).*")
+
+"""
+#define DHCPDISCOVER             1
+#define DHCPOFFER                2
+#define DHCPREQUEST              3
+#define DHCPDECLINE              4
+#define DHCPACK                  5
+#define DHCPNAK                  6
+#define DHCPRELEASE              7
+#define DHCPINFORM               8
+"""
+
+dhcp_type_re = re.compile(r".*dnsmasq-dhcp\[(?P<server_id>\d+)\]:\s+(?P<context_id>\d+)\s+"
+    "(?P<type>(DHCPDISCOVER|DHCPOFFER|DHCPREQUEST|DHCPDECLINE|DHCPACK|DHCPNAK|DHCPRELEASE|DHCPINFORM))\(eth-local\)\s+"
+    "(?P<client_ip>\d+\.\d+\.\d+\.\d+)\s+"
+    "(?P<client_mac>\w+:\w+:\w+:\w+:\w+:\w+)"
+    "(?:\s*(?P<client_hostname>\w+))?", re.VERBOSE)
+
+# Feb 12 14:45:46 val2 dnsmasq-dhcp[215151]: 2603362573 vendor class: Linux ipconfig
+dhcp_vendor_re = re.compile(r".*dnsmasq-dhcp\[(?P<server_id>\d+)\]:\s+(?P<context_id>\d+)\s+"
+    "vendor class: (?P<vendor_class>.*)")
+
+
+def get_contexts():
+    """ Gather some details of the DHCP server contexts.
+        One context spans multiple packets.
+        One packet spans multiple syslog entries.
+        Both context IDs and the key:values details we care about
+    """
+    contexts = defaultdict(dict)
+    for line in open(fn):
+        if context_match:=context_id_re.match(line):
+            d = context_match.groupdict()
+            if d['daemon']=='dnsmasq-dhcp':
+
+                d['time'] = d['time'][:7]
+                k = "{month} {date} {time} {context_id}".format(**d)
+
+                if k=='Feb 12 07:26:31 950502743':
+                    k='Feb 12 07:26:33 950502743'
+                    print(line)
+
+                # gonna look for values getting stepped on
+                old_d=contexts[k]
+
+                if (match:=dhcp_vendor_re.match(line)):
+                    contexts[k].update( match.groupdict() )
+                    # print(contexts)
+
+                if (match:=dhcp_type_re.match(line)):
+                    # context_id, client_mac, client_ip, client_hostname = match.groupdict("context_id", "client_mac", "client_ip", "client_hostname")
+                    contexts[k].update( match.groupdict() )
+                    # print(contexts)
+                    # break
+
+                    # make a list of types
+                    if "types" not in contexts[k]:
+                        contexts[k]['types'] = []
+                    contexts[k]['types'].append(contexts[k]['type'])
+                    del( contexts[k]['type'] )
+
+                # see if any old values got updated (stomp bad!)
+                for ok in old_d:
+                    if (old_d[ok] is not None) and (ok != 'types'):
+                        assert old_d[ok] == contexts[k][ok], (old_d[ok], contexts[k][ok])
+
+    return contexts
+
+
+def mac_maps():
+    """ Map MAC addresses to hostnames"""
+    mac_to_ID = defaultdict(set)
+    mac_to_IP = defaultdict(set)
+    mac_to_hostname = defaultdict(set)
+    for line in open(fn):
+        if (match:=dhcp_type_re.search(line)) is not None:
+            context_id, client_mac, client_ip, client_hostname = match.group("context_id", "client_mac", "client_ip", "client_hostname")
+            mac_to_ID[client_mac].add(_id)
+            mac_to_IP[client_mac].add(client_ip)
+            if client_hostname is not None:
+                mac_to_hostname[client_mac].add(client_hostname)
+    return mac_to_ID, mac_to_IP, mac_to_hostname
+
+def gd():
+    mac_to_ID, mac_to_IP, mac_to_hostname = mac_maps()
+    lines_by__id = group_lines_by_context_id()
+
+    #Let's see if there are more transactions for particular hosts:
+    trans_per_mac = {k: len(v) for k, v in mac_to_ID.items()}
+    trans_per_host = {mac_to_hostname.get(k, {k}).pop(): v for k, v in trans_per_mac.items()}
+    from pprint import pprint
+    pprint(trans_per_host)
+
+def is_weird(contexts):
+    # look for weird
+
+    # results:
+    # context_id is not unique. So gonna add the timetamp and see what happens.
+
+    for i in range(10):
+
+        for k in contexts:
+            print(k)
+            pprint(contexts[k])
+            if len(contexts[k]['types'] ) == i:
+                if contexts[k]['client_hostname'] in [ 'netgear', 'netgear2' ]:
+                    continue
+                else:
+                    print(k)
+                    pprint(contexts[k])
+                    print()
+                    break
+
+
+def get_log():
+    contexts = get_contexts()
+    is_weird(contexts)
+
+def main():
+    logs=get_log()
+
+
+if __name__== "__main__":
+    main()
+
+
